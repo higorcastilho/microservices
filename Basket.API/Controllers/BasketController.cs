4f0fd9b555e2d5c267914a6857e686b868470cbc
Higor Castilho <castilhohf@gmail.com>
2021-11-07 13:30:04 -0300

    first commit

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,53 @@
+﻿using Basket.API.Enitities;
+using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
+
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}
---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,64 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(productCollection => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+
+            static IEnumerable<Product> GetMyProducts() 
+            {
+                return new List<Product>()
+                {
+                    new Product()
+                    {
+                        Id = "5099803df3f4948bd2f98391",
+                        Name = "IPhone X",
+                        Description = "Beautiful IPhone",
+                        Image = "product-1.png",
+                        Price = 950.00M,
+                        Category = "Smart Phone"
+                    },
+                    new Product()
+                    {
+                        Id = "5099802df3f4948bd2f98391",
+                        Name = "Samgung 10",
+                        Description = "Beautiful Samsung",
+                        Image = "product-2.png",
+                        Price = 250.00M,
+                        Category = "Smart Phone"
+                    },
+                    new Product()
+                    {
+                        Id = "5029802df3f4948bd2f98391",
+                        Name = "TV Samsung",
+                        Description = "Beautiful TV Samsung",
+                        Image = "product-3.png",
+                        Price = 1250.00M,
+                        Category = "Smart TV"
+                    },
+                    new Product()
+                    {
+                        Id = "7099802df3f4948bd2f98391",
+                        Name = "Washing Machine",
+                        Description = "Beautiful washing machine",
+                        Image = "product-4.png",
+                        Price = 1250.00M,
+                        Category = "Home Appliances"
+                    }
+                };
+            }
+        }
+    }
+}
